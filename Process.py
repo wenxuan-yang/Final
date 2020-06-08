90e0b3e3eaf3894aa8d14fe6090c19153fe99400
Wenxuan Yang <yangwenxuan8@gmail.com>
2020-06-07 18:07:23 -0700

    Update

--- Process.py
+++ Process.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 import seaborn as sns
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 
 
 def process_data(paper):
@@ -43,22 +42,24 @@ def process_data(paper):
     return processed_data, categories
 
 
-def process_another_data(data, columns):
-    columns_lines = columns.splitlines()
-    generate_category = False
-    record_data = False
-    data
-    categories = []
-    for each in columns_lines:
-        line = each.strip()
-        if len(line) != 0:
-            if line == 'Columns     Variable':
-                generate_category = True
-            elif line == "'</PRE>', '</P>', '<P>'":
-                generate_category = False
-            elif generate_category:
-                categories.append(line)
-    print(categories)
+def correlation(data, item):
+    """
+    Returns the correlation coefficient base on the given column and the body
+    fat.
+    :param data: The complete data set.
+    :param item: The column that would be used to compute.
+    :return: The correlation coefficient of the provided column and body fat.
+    """
+    x_mean = data[item].mean()
+    y_mean = data["Percent body fat from Siri's (1956) equation"].mean()
+    sxy_abbreviated = ((data[item] - x_mean) *
+                       (data["Percent body fat from Siri's (1956) equation"]
+                        - y_mean)).sum()
+    sax_abbreviated = ((data[item] - x_mean)**2).sum()
+    syy_abbreviated = ((data["Percent body fat from Siri's "
+                             "(1956) equation"] - y_mean)**2).sum()
+    r = sxy_abbreviated / np.sqrt(sax_abbreviated) / np.sqrt(syy_abbreviated)
+    return r
 
 
 def correlation_chart(data, columns):
@@ -78,10 +79,6 @@ def correlation_chart(data, columns):
     all_correlation = sorted(all_correlation.items(), key=lambda x: x[1],
                              reverse=True)
     return all_correlation
-
-
-def calculate_bmi(data):
-    data['BMI'] = data['Weight (lbs)'] / data['Height (inches)']**2
 
 
 def graphs(data):
@@ -115,20 +112,20 @@ def linear_regression_fit(x, y):
     return reg
 
 
+#def all_data_regression(data):
+
+
+
 def main():
     sns.set(font_scale=0.7)
     url = 'http://lib.stat.cmu.edu/datasets/bodyfat'
-    url_another_data = 'http://jse.amstat.org/datasets/body.dat.txt'
-    url_another_columns = 'http://jse.amstat.org/v11n2/datasets.heinz.html'
-    process_another_data(requests.get(url_another_data).text,
-                         requests.get(url_another_columns).text)
     data, columns = process_data(requests.get(url).text)
-    # url_another_data = 'http://jse.amstat.org/datasets/body.dat.txt'
-    calculate_bmi(data)
-    # print(correlation_chart(data, columns))
-    print('Correlation coefficient:', correlation(data, 'BMI'))
+
+    print(correlation_chart(data, columns))
     graphs(data)
-    x = data.drop(["Percent body fat from Siri's (1956) equation"], axis=1).to_numpy()
+    print(data)
+    learn_data = data.drop(["Density determined from underwater weighing"], axis=1)
+    x = learn_data.drop(["Percent body fat from Siri's (1956) equation"], axis=1).to_numpy()
     y = data["Percent body fat from Siri's (1956) equation"].to_numpy()
     # train : dev : test = 7 : 1.5 : 1.5
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
@@ -137,6 +134,20 @@ def main():
     linear_reg = linear_regression_fit(x_train, y_train)
     print('MSE for train:', mean_squared_error(y_train, linear_reg.predict(x_train)))
     print('MSE for dev  :', mean_squared_error(y_dev, linear_reg.predict(x_dev)))
+
+    x_high_correlation = data[['Abdomen 2 circumference (cm)',
+                               'Chest circumference (cm)',
+                               'Hip circumference (cm)',
+                               'Weight (lbs)']].copy()
+    x_high_train, x_high_test, y_train, y_test = \
+        train_test_split(x_high_correlation, y, test_size=0.3, random_state=1)
+    x_high_dev, x_high_test, y_dev, y_test = \
+        train_test_split(x_high_test, y_test, test_size=0.5, random_state=1)
+    high_correlation_regression = linear_regression_fit(x_high_train, y_train)
+    print('MSE for high train:',
+          mean_squared_error(y_train, high_correlation_regression.predict(x_high_train)))
+    print('MSE for high dev  :',
+          mean_squared_error(y_dev, high_correlation_regression.predict(x_high_dev)))
 
 
 if __name__ == '__main__':
